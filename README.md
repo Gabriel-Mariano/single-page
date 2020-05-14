181fc324bd376f5be3c664800efb5fd7f7187249
Gabriel Mariano <gabrielmarianoo93@gmail.com>
2020-05-14 11:07:46 -0300

    Construído header para versão smartphone

---
+++ README.md
@@ -0,0 +1,6 @@
+# single-page
+  Aplicativo de Página Única - SPA
+
+## O que é um SPA
+   SPA é uma aplicação web ou site que consiste de uma única página web com o objetivo de fornecer uma experiência do usuário similar à de um aplicativo desktop. 
+   Em um SPA, todo o código necessário - HTML, JavaScript, e CSS
---
+++ src/components/header/index.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import {FaBars} from 'react-icons/fa';
+import identidadeVisual from '../../assets/identidadeVisual.png';
+import {MdClose} from 'react-icons/md';
+import {Link} from 'react-router-dom';
+import './style.css';
+
+
+function Header(){
+
+  const [bars,setBars] = useState(true);
+    
+  function Show() {
+    setBars(false);
+  }
+
+  function Hide(){
+      setBars(true);
+  }
+
+    return(
+        <header>
+            <figure className="symbol"><img src={identidadeVisual} alt="Logo"/></figure>    
+            <button id="btn-open" className="btnActions" 
+                onClick={Show}
+                style={ {display: bars?'block':'none'} }>
+                <FaBars style={{ color: 'white' }} size="1.5em"></FaBars>
+            </button>
+            <nav id="menu-bars"
+                 style={ {display: bars?'none':'block'} }
+                >
+                <button id="btn-close" className="btnActions"
+                    onClick={Hide} >
+                    <MdClose style={{ color: 'white' }} size="1.8em"></MdClose>
+                </button>
+                <ul>
+                    <li><Link>Home</Link></li>
+                    <li><Link>Sobre</Link></li>
+                    <li><Link>Projetos</Link></li>
+                    <li><Link>Budget</Link></li>
+                    <li><Link>Contato</Link></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
