b11978b4ad9dc62343126ea1383f6ee0c35fe13f
Gabriel Mariano <gabrielmarianoo93@gmail.com>
2020-05-26 00:53:18 -0300

    Personalizando a secao info

--- src/components/sections/section.js
+++ src/components/sections/section.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import './section.css';
+import Brainstorm from '../../assets/brainstorm.png';
+import Templates from '../../assets/template.png';
+import Blog from '../../assets/blog.png';
+
 
 
 function Section(){
     return(
-        <div className="info" id="info">
-            
-        </div>
+        <section className="info" id="info">
+            <article>
+                <img src={Brainstorm} alt=""/>
+                <h1>Brainstorm</h1>
+                <h2>Tempestade de ideias</h2>
+                <button type="button">Saiba Mais</button>
+            </article>
+            <article>
+                <img src={Templates} alt=""/>
+                <h1>TEMPLATES</h1>
+                <h2>Desenvolvemos o rosto do seu site</h2>
+                <button type="button">Saiba Mais</button>
+            </article>
+            <article>
+                <img src={Blog} alt=""/>
+                <h1>BLOG</h1>
+                <h2>Nosso Blog</h2>
+                <button type="button">Saiba Mais</button>
+            </article>
+        </section>
     );
 }
 
